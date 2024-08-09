3ea34f43e69e5c43eccc9d70b326c16b21de7520
Guilherme Castro <54508436+Guipzss@users.noreply.github.com>
2024-08-09 14:28:49 -0300

    range.py

--- range.py
+++ range.py
@@ -2,7 +2,7 @@ a = int(input("Digite o primeiro número: "))
 b = int(input("Digite o segundo número: "))
 soma = 0
 if a < b:
-    for termo in range(a, b):
+    for termo in range(a, b+1):
         soma += termo
     print(soma)
 else:
